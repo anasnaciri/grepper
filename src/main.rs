bbe84435123c712df88ff5c4b3cde15148e2453d
nacirianas <12anas2000@gmail.com>
2026-05-11 23:30:17 +0100

    feat: case sensitive/insesitive search support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use grepper::search;
+use grepper::{search, search_case_insensitive};
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -21,7 +21,13 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("grepper: {line}")
     }
 
@@ -31,6 +37,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -41,6 +48,12 @@ impl Config {
         // refactor this later to not use the clone() method
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
